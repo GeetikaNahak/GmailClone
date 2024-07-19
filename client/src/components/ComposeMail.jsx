b3caad5d44e288606060a9a49b4dd75a1967b71b
GeetikaNahak <geetikasaisravaninahak@gmail.com>
2024-07-19 21:27:41 +0530

    update

---
+++ client/src/components/ComposeMail.jsx
@@ -0,0 +1,173 @@
+import { useState, useEffect } from 'react';
+import { Dialog, styled, Typography, Box, InputBase, TextField, Button } from '@mui/material'; 
+import { Close, DeleteOutline } from '@mui/icons-material';
+import useApi from '../hooks/useApi';
+import { API_URLS } from '../services/api.urls';
+
+const dialogStyle = {
+    height: '90%',
+    width: '80%',
+    maxWidth: '100%',
+    maxHeight: '100%',
+    boxShadow: 'none',
+    borderRadius: '10px 10px 0 0',
+}
+
+const Header = styled(Box)`
+    display: flex;
+    justify-content: space-between;
+    padding: 10px 15px;
+    background: #f2f6fc;
+    & > p {
+        font-size: 14px;
+        font-weight: 500;
+    }
+`;
+
+const RecipientWrapper = styled(Box)`
+    display: flex;
+    flex-direction: column;
+    padding: 0 15px;
+    & > div {
+        font-size: 14px;
+        border-bottom: 1px solid #F5F5F5;
+        margin-top: 10px;
+    }
+`;
+
+const Footer = styled(Box)`
+    display: flex;
+    justify-content: space-between;
+    padding: 10px 15px;
+    align-items: center;
+`;
+
+const SendButton = styled(Button)`
+    background: #0B57D0;
+    color: #fff;
+    font-weight: 500;
+    text-transform: none;
+    border-radius: 18px;
+    width: 100px;
+`
+
+const ComposeMail = ({ open, setOpenDrawer }) => {
+    const [data, setData] = useState({ to: '', subject: '', body: '' });
+    const sentEmailService = useApi(API_URLS.saveSentEmails);
+    const saveDraftService = useApi(API_URLS.saveDraftEmails);
+
+    const config = {
+        Username: process.env.REACT_APP_USERNAME,
+        Password: process.env.REACT_APP_PASSWORD,
+        Host: 'smtp.elasticemail.com',
+        Port: 2525,
+    }
+
+    useEffect(() => {
+        console.log('Username:', process.env.REACT_APP_USERNAME);
+        console.log('Password:', process.env.REACT_APP_PASSWORD);
+    }, []);
+
+    const onValueChange = (e) => {
+        setData({ ...data, [e.target.name]: e.target.value });
+    }
+
+    const sendEmail = async (e) => {
+        e.preventDefault();
+
+        if (window.Email) {
+            try {
+                const message = await window.Email.send({
+                    ...config,
+                    To: data.to,
+                    From: "geetikanahak@gmail.com",
+                    Subject: data.subject,
+                    Body: data.body
+                });
+                alert(message);
+
+                const payload = {
+                    to: data.to,
+                    from: "geetikanahak@gmail.com",
+                    subject: data.subject,
+                    body: data.body,
+                    date: new Date(),
+                    image: '',
+                    name: 'Code for Interview',
+                    starred: false,
+                    type: 'sent'
+                };
+
+                await sentEmailService.call(payload);
+
+                if (!sentEmailService.error) {
+                    setOpenDrawer(false);
+                    setData({ to: '', subject: '', body: '' });
+                } else {
+                    console.error("Error sending email: ", sentEmailService.error);
+                }
+            } catch (error) {
+                console.error("Error sending email: ", error);
+            }
+        }
+    }
+
+    const closeComposeMail = async (e) => {
+        e.preventDefault();
+
+        const payload = {
+            to: data.to,
+            from: "geetikanahak@gmail.com",
+            subject: data.subject,
+            body: data.body,
+            date: new Date(),
+            image: '',
+            name: 'Code for Interview',
+            starred: false,
+            type: 'drafts'
+        };
+
+        try {
+            await saveDraftService.call(payload);
+
+            if (!saveDraftService.error) {
+                setOpenDrawer(false);
+                setData({ to: '', subject: '', body: '' });
+            } else {
+                console.error("Error saving draft: ", saveDraftService.error);
+            }
+        } catch (error) {
+            console.error("Error saving draft: ", error);
+        }
+    }
+
+    return (
+        <Dialog
+            open={open}
+            PaperProps={{ sx: dialogStyle }}
+        >
+            <Header>
+                <Typography>New Message</Typography>
+                <Close fontSize="small" onClick={(e) => closeComposeMail(e)} />
+            </Header>
+            <RecipientWrapper>
+                <InputBase placeholder='Recipients' name="to" onChange={onValueChange} value={data.to} />
+                <InputBase placeholder='Subject' name="subject" onChange={onValueChange} value={data.subject} />
+            </RecipientWrapper>
+            <TextField 
+                multiline
+                rows={20}
+                sx={{ '& .MuiOutlinedInput-notchedOutline': { border: 'none' } }}
+                name="body"
+                onChange={onValueChange}
+                value={data.body}
+            />
+            <Footer>
+                <SendButton onClick={sendEmail}>Send</SendButton>
+                <DeleteOutline onClick={() => setOpenDrawer(false)} />
+            </Footer>
+        </Dialog>
+    )
+}
+
+export default ComposeMail;
