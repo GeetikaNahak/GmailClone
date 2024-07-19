b0be5c4848ccf9f1c7192b1f97715c60b85c9ac1
GeetikaNahak <geetikasaisravaninahak@gmail.com>
2024-07-19 21:32:59 +0530

    update

--- server/database/db.js
+++ server/database/db.js
@@ -7,7 +7,7 @@ const USERNAME = process.env.DB_USERNAME;
 const PASSWORD = process.env.DB_PASSWORD; 
 
 const Connection = () => {
-    const DB_URI = `mongodb+srv://geetika:gmailclone@gmailclone.u3qcwoc.mongodb.net/?retryWrites=true&w=majority&appName=gmailclone`;
+    const DB_URI = `mongodb+srv://${USERNAME}:${PASSWORD}@gmailclone.u3qcwoc.mongodb.net/?retryWrites=true&w=majority&appName=gmailclone`;
     try {
         mongoose.connect(DB_URI, { useNewUrlParser: true });
         mongoose.set('strictQuery', false);
